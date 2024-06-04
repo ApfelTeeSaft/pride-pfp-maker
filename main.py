eb55e22cdf2594d40b2aa5fe8bbbef2aa5406b4f
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-06-04 09:43:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def serve_subroute():
     return send_from_directory(os.getcwd(), 'index.html')
 
+@app.route('/')
+def main():
+    return send_from_directory(os.path.join(app.root_path, 'main'), 'index.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
